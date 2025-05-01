5b8ab39fbee5d0e3d8b051b4976b60caf59baf07
sqr2007 <alkthyrysqr19@gmail.com>
2025-05-01 19:11:47 +0300

    MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,19 @@
+package com.daleeli.specialtyapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.start_button)
+        startButton.setOnClickListener {
+            val intent = Intent(this, QuestionActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
